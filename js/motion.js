5dbb37b4389766ac1710a654f648693281788e03
hankakukatakana <65936854+hankakukatakana@users.noreply.github.com>
2023-02-21 15:34:17 +0900

    d

--- js/motion.js
+++ js/motion.js
@@ -2,13 +2,7 @@ const pics_src = ["image/works/work (1).png","image/works/work (2).png","image/w
 let num = -1;
 
 function slideshow_timer(){
-  if (num === 2){
-    num = 0;
-  } 
-  else {
-    num ++;
+    num = Math.floor(Math.random()*pics_src.length);
+    document.getElementById("mypic").src = pics_src[num];
   }
-  document.getElementById("mypic").src = pics_src[num];
-}
-
-setInterval(slideshow_timer, 1000);
+  setInterval(slideshow_timer,1000); 
